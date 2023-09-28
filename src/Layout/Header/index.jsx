582d1a45337748a0eb7f319e073356c26bd5d151
Ibnul Ashir <ibnulashir42@gmail.com>
2023-09-29 00:03:41 +0600

    header fixed

--- src/Layout/Header/index.jsx
+++ src/Layout/Header/index.jsx
@@ -1,47 +1,26 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, NavLink, useLocation, useNavigate } from 'react-router-dom';
 
 const Header = ({ open, setOpen }) => {
   const navigate = useNavigate()
   const location = useLocation()
   const links = [
-    {
-      name: "Network Installation",
-      link: "/network-intallation",
-      child: [
-        { name: "Network Installation 1", link: "/network-intallation" },
-        { name: "Network Installation 2", link: "/network-intallation" },
-        { name: "Network Installation 3", link: "/network-intallation" }
-      ]
-    },
-    {
-      name: "Security",
-      link: "/",
-      child: [
-        { name: "Security 1", link: "/security-network" },
-        { name: "Security 2", link: "/security-network" },
-        { name: "Security 3", link: "/security-network" }
-      ]
-    },
-    {
-      name: "EoT",
-      link: "/",
-      child: [
-        { name: "EoT 1", link: "/eot-network" },
-        { name: "EoT 2", link: "/eot-network" },
-        { name: "EoT 3", link: "/eot-network" }
-      ]
-    },
-    { name: "broadband", link: "/broadband-network" },
-    {
-      name: "courses",
-      link: "/",
-      child: [
-        { name: "course 1", link: "/course1" },
-        { name: "course 2", link: "/course2" },
-        { name: "course 3", link: "/course3" }
-      ]
-    },
+    { name: "UniFi Gateway Console", link: "/network-intallation" },
+    { name: "Wifi", link: "/wifi" },
+    { name: "Switching", link: "/eot-network" },
+    { name: "Cameras & Security", link: "/security-network" },
+    { name: "New integrations", link: "/new-integrations" },
+    { name: "accessory tech", link: "/" },
+    { name: "Identity", link: "/identity" }
+    // {
+    //   name: "Network Installation",
+    //   link: "/network-intallation",
+    //   child: [
+    //     { name: "Network Installation 1", link: "/network-intallation" },
+    //     { name: "Network Installation 2", link: "/network-intallation" },
+    //     { name: "Network Installation 3", link: "/network-intallation" }
+    //   ]
+    // }
   ]
 
 
@@ -111,15 +90,16 @@ const Header = ({ open, setOpen }) => {
 
   return (
     <div className='w-full'>
-      <div className={`hidden lg:block h-8 ${whitebackground ? 'bg-white' : 'bg-gradient-to-b'} from-[#01042E] to-[#080B34] via-[#040730]`}></div>
-      <div className={`hidden lg:block ${whitebackground ? 'bg-white' : 'bg-gradient-to-b'} from-[#01042C] to-[#01032A] -my-1`}>
+      <div className={`hidden xl:block h-8 ${whitebackground ? 'bg-white' : 'bg-gradient-to-b'} from-[#01042E] to-[#080B34] via-[#040730]`}></div>
+      <div className={`hidden xl:block ${whitebackground ? 'bg-white' : 'bg-gradient-to-b'} from-[#01042C] to-[#01032A] -my-1`}>
         <div className={`max-w-7xl mx-auto flex justify-between ${whitebackground ? 'text-black' : 'text-white'}`}>
           <div className='flex gap-5'>
-            <div className="relative w-20 h-20">
-              <div onClick={logoClickHandler} className="absolute left-0 -top-0 w-full h-full cursor-pointer">
-                <video className="logo-video" autoPlay muted loop='true'>
+            <div className="relative px-5">
+              <div onClick={logoClickHandler} className="absolute left-0 -top-0 w-full h-full flex justify-center items-center cursor-pointer">
+                {/* <video className="logo-video" autoPlay muted loop='true'>
                   <source src="/assets/animated_logo.mp4" type="video/mp4" />
-                </video>
+                </video> */}
+                <p className='-mt-2 font-extrabold'>Logo</p>
               </div>
             </div>
             <ul className="flex gap-4 pt-3" ref={headerRef}>
@@ -130,7 +110,10 @@ const Header = ({ open, setOpen }) => {
                 >
                   {link.child ?
                     <button onClick={() => handleClick(index)} className='capitalize flex gap-1 items-center'>{link.name} {link.child && <img src={`/assets/icons/arrow${whitebackground ? '-black' : ''}.svg`} alt="store" className={`w-3 h-3 pt-1 transition-all ${currentLink === index ? 'rotate-180' : ''}`} />}</button> :
-                    <Link to={link.link} onClick={() => setCurrentLink(null)} className='capitalize flex gap-1 items-center'>{link.name}</Link>}
+                    <NavLink to={link.link} onClick={() => setCurrentLink(null)}
+                      // className='capitalize flex gap-1 items-center hover:text-[rgba(41,88,255,0.7)] transition-all duration-300'
+                      className={({ isActive }) =>  isActive ? "capitalize flex gap-1 items-center text-[rgba(41,88,255,0.7)] transition-all duration-300" : "capitalize flex gap-1 items-center hover:text-[rgba(41,88,255,0.7)] transition-all duration-300" }
+                    >{link.name}</NavLink>}
                   <ul className={`absolute mt-2 flex flex-col bg-[#01042E] text-white transition-opacity duration-500 ${currentLink === index ? 'opacity-100' : 'opacity-0'}`}>
                     {link?.child?.map(childLink => (
                       <Link onClick={() => setCurrentLink(null)} key={childLink.name} to={childLink.link} className={`py-2 px-2 hover:bg-pink-600 ${currentLink === index ? 'block' : 'hidden'}`}>{childLink.name}</Link>
@@ -148,7 +131,7 @@ const Header = ({ open, setOpen }) => {
           </div>
         </div>
       </div>
-      <div className="lg:hidden bg-[#FFFFFF] py-2 flex flex-col z-50">
+      <div className="xl:hidden bg-[#FFFFFF] py-2 flex flex-col z-50">
         <div className="flex justify-between items-center px-2">
           <div className="h-10 w-10 bg-pink-200"></div>
           <div onClick={() => setOpen(!open)} className="bg-[rgba(255,255,255,1)] shadow-[0_0_40px_0_rgba(0,0,0,0.15)] h-9 w-9 rounded-md flex flex-col gap-[6px] justify-center items-center cursor-pointer">
@@ -157,12 +140,13 @@ const Header = ({ open, setOpen }) => {
             <div className={`w-5 h-[2px] rounded-full bg-[rgba(32,32,32,1)] transition-all duration-300 ${open && '-rotate-45 w-7 -mt-[8px] -ml-[1px]'}`}></div>
           </div>
         </div>
-        <div className="relative flex items-center justify-center">
+        <div className="relative flex items-center justify-center text-white">
           <div className={`absolute top-1 flex-1 transition-all delay-150 bg-gradient-to-b from-[#01042C] to-[#01032A] h-full w-full ${open ? 'h-screen left-0' : '-left-[100vw]'}`}>
-            <ul className="flex flex-col gap-4 pt-3 px-2">
+            <ul className="flex flex-col gap-4 pt-3 ">
               {links.map((link, index) => (
                 <li
                   key={link.name}
+                  className='border-b px-2 my-1'
                 >
                   {link.child ?
                     <button onClick={() => handleClickMobile(link, index)} className='capitalize flex gap-1 items-center'>{link.name} {link.child && <img src="/assets/icons/arrow.svg" alt="store" className={`w-3 h-3 pt-1 transition-all ${currentIndex === index ? 'rotate-180' : ''}`} />}</button> :
