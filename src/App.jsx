87dfabaf04b9742e5f0e8bb5f644abfd61304890
Ibnul Ashir <ibnulashir42@gmail.com>
2023-11-21 09:38:39 +0600

    scroll problem fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { useLocation } from 'react-router-dom';
 import Layout from './Layout'
+import { useEffect } from 'react';
 
 function App() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // Scroll to top on page load
+    window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+  }, [pathname]);
 
   return (
-    <div className="font-roboto">
+    <div className="font-roboto scroll-smooth">
       <Layout />
     </div>
   )
--- src/Layout/index.jsx
+++ src/Layout/index.jsx
@@ -2,26 +2,14 @@ import React, { useEffect, useState } from 'react'
 import Header from './Header'
 import Content from './Content'
 import Footer from './Footer'
-import { Routes } from 'react-router-dom'
-import { useLocation } from 'react-router-dom';
-
-function ScrollToTop() {
-  const { pathname } = useLocation();
-
-  useEffect(() => {
-    window.scrollTo(0, 0);
-  }, [pathname]);
-
-  return null;
-}
 
 const Layout = () => {
   const [open, setOpen] = useState(false)
   return (
-    <div className={`w-screen h-screen overflow-x-hidden ${open ? 'overflow-y-hidden' : 'overflow-y-auto'} flex flex-col`}>
+    <div className={`flex flex-col ${open ? "h-screen overflow-y-hidden md:min-h-screen md:h-auto md:overflow-y-visible" : ""} min-h-screen overflow-y-auto overflow-x-hidden`}>
+      {/* <div className={`w-screen h-screen overflow-x-hidden ${open ? 'overflow-y-hidden' : 'overflow-y-auto'} flex flex-col`}> */}
       <Header open={open} setOpen={setOpen} />
       <div className="flex-1">
-        <ScrollToTop />
         <Content />
       </div>
       <Footer />
